2358e3a063b899befb3b90a49df12944fa1be303
Xiang Lin <1748968@uab.cat>
2025-06-11 19:09:28 +0200

    ajuste en configuracion

--- 0. C++ Code/Logic Game/joc.cpp
+++ 0. C++ Code/Logic Game/joc.cpp
@@ -40,14 +40,32 @@ bool Joc::actualitza(int mousePosX, int mousePosY, bool mouseStatus)
 	//TODO 1: Interactuar amb la crida per dibuixar gràfics (sprites).
 	// 	      Dibuixar a pantalla el fons i el gràfic amb el tauler buit.
 	//------------------------------------------------------------------
+    GraphicManager* gManager = GraphicManager::getInstance();
+    gManager->drawSprite(GRAFIC_FONS, 0, 0);
+    gManager->drawSprite(GRAFIC_TAULER, POS_X_TAULER, POS_Y_TAULER);
 
-	//TODO 1.1 Afegir l'include de GraphicManager --> #include "GraphicManager.h"
-	//TODO 1.2 Fer la crida de dibuixar un sprite --> GraphicManager::getInstance()->drawSprite(image, posX, posY);
-	//	    Per començar podem cridar el drawSprite amb els params --> 
-	//          (GRAFIC_FONS,0,0) i 
-	//          (GRAFIC_TAULER, POS_X_TAULER, POS_Y_TAULER)
+    int fila = 2;      // Ejemplo: fila 2 (0-7)
+    int columna = 0;   // Ejemplo: columna 3 (0-7)
 
- 
+    // 4. Calcular la posición (x, y) de la ficha en píxeles
+    float posX = POS_X_TAULER + CASELLA_INICIAL_X + columna * AMPLADA_CASELLA;
+    float posY = POS_Y_TAULER + CASELLA_INICIAL_Y + fila * ALCADA_CASELLA;
+
+    int posTextX = POS_X_TAULER;
+    int posTextY = POS_Y_TAULER + (ALCADA_CASELLA * NUM_FILES_TAULER) + 120;
+    std::string msg = "PosX: " + to_string(mousePosX) + ", PosY: " + to_string(mousePosY);
+    GraphicManager::getInstance()->drawFont(FONT_WHITE_30, posTextX, posTextY, 0.8, msg);
+
+    if ((mouseStatus) && (mousePosX >= (POS_X_TAULER + CASELLA_INICIAL_X)) &&
+        (mousePosY >= POS_Y_TAULER + CASELLA_INICIAL_Y) &&
+        (mousePosX <= (POS_X_TAULER + CASELLA_INICIAL_X + AMPLADA_CASELLA * NUM_COLS_TAULER)) &&
+        (mousePosY <= (POS_Y_TAULER + CASELLA_INICIAL_Y + ALCADA_CASELLA * NUM_FILES_TAULER)))
+    {
+        int posX = POS_X_TAULER + CASELLA_INICIAL_X + ((columna) * AMPLADA_CASELLA);
+        int posY = POS_Y_TAULER + CASELLA_INICIAL_Y + ((fila) * ALCADA_CASELLA);
+        GraphicManager::getInstance()->drawSprite(GRAFIC_FITXA_BLANCA, posX, posY);
+    }
+
 
 
 	//TODO 1.3: Dibuixar a pantalla el gràfic amb el tauler i una fitxa blanca a la posició (fila, columna ) del tauler
